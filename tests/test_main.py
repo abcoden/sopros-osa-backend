dbd24ece0d5606e18c75f20579388421075b1753
abcoden <abcoden@proton.me>
2024-04-01 21:20:55 +0200

    add logic and tests for invalidates rules

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 
 from sopros_osa_backend.main import app
 from sopros_osa_backend.model import SoprosCountry
+from sopros_osa_backend.model import SoprosRule
 from sopros_osa_backend.model import SoprosStatus
 
 
@@ -34,3 +35,23 @@ def test_read_states():
         status_list = [SoprosStatus(**item) for item in items_json]
         assert status_list != []
         assert any(item.id == "MIL" for item in status_list) == True
+
+def test_calc_success_GER_CIT():
+    with TestClient(app) as client:
+        response = client.post("/api/calc/GER", content='["CIT"]')
+        print(response)
+        assert response.status_code == 200
+        items_json = response.json()
+        rule_list = [SoprosRule(**item) for item in items_json]
+        assert len(items_json) == 10
+        assert any(item.id == "MAT_MINCOV_CIT" for item in rule_list) == True
+
+def test_calc_success_invalidates_GER_CIT_MIL():
+    with TestClient(app) as client:
+        response = client.post("/api/calc/GER", content='["CIT", "MIL"]')
+        print(response)
+        assert response.status_code == 200
+        items_json = response.json()
+        rule_list = [SoprosRule(**item) for item in items_json]
+        assert len(items_json) == 25
+        assert any(item.id == "MAT_MINCOV_CIT" for item in rule_list) == False
